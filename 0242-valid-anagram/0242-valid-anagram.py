2bc66dc956b75b718441be2d99c607ea146253c8
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-12 22:48:50 +0530

    Time: 13 ms (49.7%), Space: 18 MB (50.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,13 +6,18 @@ class Solution:
         if len(s) != len(t):
             return False
         
+        # using default dict as hash map, so that values are defaulted to 0
         hashMap = defaultdict(int)
         for i in range(len(s)):
             hashMap[s[i]] += 1
             hashMap[t[i]] -= 1
         
+        # check if character count of s and t matches
         for key in hashMap:
             if(hashMap[key] != 0):
                 return False
         
         return True
+
+# Time complexity: O(n)
+# Space complexity: O(1) - since only lowercase letters, max 26
