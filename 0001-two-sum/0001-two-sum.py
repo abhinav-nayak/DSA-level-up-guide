bffd48c2880851ba39876078524f457fe3cea31d
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-13 18:50:18 +0530

    Time: 1 ms (58.62%), Space: 19.1 MB (10.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,10 +3,13 @@ class Solution:
         # hash map for O(1) lookup
         hashMap = dict()
 
+        # iterate and find if suitable pair exists, else update hash map
         for i, n in enumerate(nums):
             num2 = hashMap.get(target-n)
+            # NOTE: if num2 is 0, then just 'if num2:' condition won't work
             if num2 is not None:
                 return i, num2
             hashMap[n] = i
 
-        
+# Time complexity: O(n)      
+# Space complexity: O(n)
