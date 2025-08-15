ed3d747294b3c8d1908a5e50d354fb5909d84f77
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-15 12:29:42 +0530

    Time: 65 ms (73.7%), Space: 26.7 MB (94.71%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # This feels more like a two pointer approach than sliding window
+
+        maxProfit=0
+        if len(prices)==1:
+            return maxProfit
+        
+        # initialise 2 pointers, 'b' for buying and 's' for selling
+        b, s=0, 1
+        while s<len(prices):
+            if prices[s]<prices[b]:
+                # since selling price is less, we can buy stock at this price
+                b=s
+                s+=1
+            else:
+                # calulate profit and move sforward to check for better profits
+                profit=prices[s]-prices[b]
+                maxProfit=profit if profit>maxProfit else maxProfit
+                s+=1
+        
+        return maxProfit
+
+# Time complexity: O(n)
+# Space comlexity: O(1)
