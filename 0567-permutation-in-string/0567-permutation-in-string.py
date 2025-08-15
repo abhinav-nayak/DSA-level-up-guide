ac8246fc66c18cbfc3cef8ae732b02dafaa2ced1
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-15 19:00:44 +0530

    Time: 35 ms (43.14%), Space: 17.9 MB (39.81%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,45 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1)>len(s2):
+            return False
+
+        # create hash map of s1
+        count1=[0]*26
+        for c in s1:
+            count1[ord(c)-ord("a")]+=1
+
+        # create hash map of substring of s2. Substring length equal to length of s1.
+        count2=[0]*26
+        for i in range(len(s1)):
+            count2[ord(s2[i])-ord("a")]+=1
+
+        # Maintain count of matching letters
+        letterCountMatch=0
+        for i in range(len(count1)):
+            if count1[i]==count2[i]:
+                letterCountMatch+=1
+        
+        # sliding window apporach to find a permutation
+        l=0
+        for r in range(len(s1), len(s2)):
+            if letterCountMatch==26:
+                break
+
+            # increment r pointer
+            if count2[ord(s2[r])-ord("a")] == count1[ord(s2[r])-ord("a")]:
+                letterCountMatch-=1
+            count2[ord(s2[r])-ord("a")]+=1
+            if count2[ord(s2[r])-ord("a")] == count1[ord(s2[r])-ord("a")]:
+                letterCountMatch+=1
+            
+            # increment left pointer
+            if count2[ord(s2[l])-ord("a")] == count1[ord(s2[l])-ord("a")]:
+                letterCountMatch-=1
+            count2[ord(s2[l])-ord("a")]-=1
+            if count2[ord(s2[l])-ord("a")] == count1[ord(s2[l])-ord("a")]:
+                letterCountMatch+=1   
+            l+=1
+
+        return letterCountMatch==26
+ 
