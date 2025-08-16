08a46859e75757f259b21d44ed01dfa25b586970
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-16 20:04:25 +0530

    Time: 123 ms (37.9%), Space: 33.7 MB (19.31%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,29 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Basically at each index, we have to find the next maximum.
+        # This is a type of 'next maximum/ next minimum' problem.
+        # Hence, we can choose stack
+
+        # initialize a stack.
+        # Each element in stack is tuple: (<index of temp>, <temp>)
+        # we need index to calculate number of days
+        stack=[]
+
+        #iterate over the input temperatures
+        answer=[0]*len(temperatures)
+        for i, temp in enumerate(temperatures):
+            '''
+            if len(stack)==0 or temp<=stack[-1][1]:
+                stack.append(tuple(i, temp))
+            else:
+            '''
+            # keep popping till the temperature is more than top of the stack
+            while stack and temp>stack[-1][1]:
+                t=stack.pop()
+                answer[t[0]]=i-t[0]
+            stack.append((i, temp))
+        
+        # Remaining elements in the stack are inincreasong order such that there are no warmer days.
+        # Hence, default value of 0 is valid
+        return answer
+        
