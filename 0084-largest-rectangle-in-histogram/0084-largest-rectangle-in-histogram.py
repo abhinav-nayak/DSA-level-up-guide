71f7f0a28301f5b2db1898c286244b32d8dd9e42
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-17 00:20:08 +0530

    Time: 142 ms (89.1%), Space: 35.8 MB (27.88%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -0,0 +1,29 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # Every element is dependent on next element to know if it can extend
+        # for area calculation. Hence, we can think of stack.
+
+        maxArea=0
+
+        # stack to store: (<starting index for that height>, <height>)
+        stack=[]
+        for i, height in enumerate(heights):
+            startIndex=i
+            while stack and height<stack[-1][1]:
+                # case where we cannot extend anymore
+                lastPoppedIndex=stack[-1][0]
+                area=(i-lastPoppedIndex)*stack[-1][1]
+                maxArea=area if area>maxArea else maxArea
+                stack.pop()
+                startIndex=lastPoppedIndex
+
+            # Possible of extending for area calculation               
+            stack.append((startIndex, height))
+        
+        # Now stack is in increasing order only
+        while stack:
+            area=(len(heights)-stack[-1][0])*stack[-1][1]
+            maxArea=area if area>maxArea else maxArea
+            stack.pop()            
+        
+        return maxArea
