e04e1d29a5ed58c60f1376b0b6023a70f0ec2b65
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-19 22:37:58 +0530

    Time: 2 ms (15.11%), Space: 17.9 MB (17.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -12,13 +12,19 @@ class Solution:
         # readjust the links till you reach end of any one of the list
         while list1 and list2:
             if list1.val <= list2.val:
+                # pick element from list 1
                 prev.next=list1
                 prev=list1
                 list1=list1.next
             else:
+                # pick element from list 2
                 prev.next=list2
                 prev=list2
                 list2=list2.next
         
         prev.next=list1 if list1 else list2
         return head.next
+
+# Time complexity: O(m+n)
+# Space complexity: O(1)
+# where n is the length of list 1 and m in the length of list 2
