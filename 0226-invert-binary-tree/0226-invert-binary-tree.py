01905e9830881cc48a1943a38f288d711120e0b3
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-27 11:24:34 +0530

    Time: 0 ms (100%), Space: 17.9 MB (9.46%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # since we have to swap the left and right of each node from bottom to top,
+        # we can think of DFS traversal. Post-order traversal can be used because once we
+        # swap the left and right sub-trees recursively, the root's left and right should be swapped.
+
+        # Post-order traversal
+
+        # base condition: if the node has no children, there is nothing to swap
+        if (not root) or (not root.left and not root.right):
+            return root
+        
+        leftNode=self.invertTree(root.left)
+        rightNode=self.invertTree(root.right)
+        root.left=rightNode
+        root.right=leftNode
+
+        return root
