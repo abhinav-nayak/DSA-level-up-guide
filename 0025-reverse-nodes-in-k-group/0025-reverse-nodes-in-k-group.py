59a8cb0ebd35e175642d7cf1127aa3e87b995ba1
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-29 19:12:22 +0530

    Time: 0 ms (100%), Space: 18.5 MB (60.05%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Iterate through the list using lead-lag pointers.
+        # lead-lag pointers should have a distance of k between them.
+        # Once lead-lag pointers have a distance k between them, reverse the sub-list.
+
+        dummyHead=ListNode(next=head)
+        lead, lag = head, head
+        prev = dummyHead
+        while lead:
+            # move lead pointer k steps ahead
+            n=k
+            while n>0 and lead:
+                lead=lead.next
+                n-=1
+            
+            if n==0:
+                # we have found a sublist of length k. Now, reverse them.
+                prev2=None
+                nextPrev=lag
+                while lag != lead:
+                    nextt=lag.next
+                    lag.next=prev2
+                    prev2=lag
+                    lag=nextt
+                
+                prev.next=prev2
+                prev=nextPrev
+            elif not lead:
+                prev.next=lag
+
+        return dummyHead.next
+        
