057fa880c5122cc571e30f5cb759ced4abe79399
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-08-31 11:47:06 +0530

    Time: 0 ms (100%), Space: 21 MB (93.2%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # Inorder DFS traversal of Binary Search Tree will always be in sorted order.
+        # We can use iterative in-order tarversal approach to traverse the BST and return the
+        # kth smallest element.
+        
+        # recursive method using in-build recursion stack.
+        # For iterative approach, we need to use a stack manually.
+        stack = []
+        curr = root
+
+        while curr or stack:
+            # keep pushing nodes to stack till you reach left most None node
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+            # pop node from stack and print value - inorder traversal
+            curr = stack.pop()
+
+            k -= 1
+            if k==0:
+                return curr.val
+            
+            curr = curr.right
+
+# Time complexity: O(n)
+# Space complexity: O(n)
