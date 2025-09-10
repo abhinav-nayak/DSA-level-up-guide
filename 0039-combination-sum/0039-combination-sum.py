65de7cc9a4a2bc93a18e9b5a71b995841aa4a2e8
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-09-10 23:12:32 +0530

    Time: 11 ms (57.07%), Space: 17.7 MB (87.92%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # Since we have to find 'all unique combinations', it is a hint that backtracking can be used.
+        result = []
+
+        combination = []
+        def dfs(i: int, summ: int):
+            # if summ >= target, we have hit dead-end
+            if summ >= target or i>=len(candidates):
+                if summ == target:
+                    # shallow copy is needed to prevent data corruption
+                    result.append(combination.copy())
+                return
+
+            # consider candidates[i] for solution
+            combination.append(candidates[i])
+            dfs(i, summ +candidates[i])
+            # backtrack: undo previous step of adding candidates[i] in combination
+            combination.pop()
+            # explore other paths
+            dfs(i+1, summ)
+
+        
+        dfs(0, 0)
+        return result
+        
