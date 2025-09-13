d6a941fe1bbbdddde3ae5bb223e4009f75c196a6
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-09-13 12:28:19 +0530

    Time: 0 ms (100%), Space: 17.8 MB (97.73%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # Since we have to find 'all possible' subsets, we can think of backtracking
+        result = []
+
+        # To avoid duplicate combinations like [1, 7] and [7, 1]:
+        # 1. Sort the input
+        # 2. While backtracking, if candidates[i] is popped then don't consider other values equal to 
+        # candidates[i] as all combinations with candidates[i] value is explored.
+        nums.sort()
+
+        subset = []
+        def dfs(i: int):
+            if i >= len(nums):
+                # shallow copy is needed for result data corruption
+                result.append(subset.copy())
+                return
+
+            subset.append(nums[i])
+            dfs(i+1)
+
+            # backtrack: de-select nums[i]
+            # While backtracking, if candidates[i] is popped then don't consider other values equal to 
+            # candidates[i] as all combinations with candidates[i] value is explored.
+            subset.pop()
+            while i < len(nums)-1 and nums[i] == nums[i+1]:
+                i += 1
+            dfs(i+1)
+
+        dfs(0)
+        return result
+        
