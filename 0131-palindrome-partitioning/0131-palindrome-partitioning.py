a9dafb17d2255b0d935d32982f354bf4ec11db2c
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-09-17 19:10:07 +0530

    Time: 50 ms (54.34%), Space: 34.7 MB (9.81%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,34 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        # Since we have to find multiple partitions such that substrings are palindrome, we can use backtracking
+        result = []
+
+        partition = []
+        def dfs(i: int):
+            # if we have reached the end, append to result
+            if i >= len(s):
+                result.append(partition.copy())
+                return
+            
+            # check if s[i:j+1] is palindrome. If yes, continue further to search substring that are palindrome.
+            for j in range(i, len(s)):
+                if self.isPalindrome(s, i, j):
+                    partition.append(s[i:j+1])
+                    dfs(j+1)
+                    partition.pop()
+        
+        dfs(0)
+        return result
+        
+    def isPalindrome(self, s: str, i: int, j: int):
+        """
+        Checks if the provided string is palindrome
+        """
+        palindrome = True
+        while i <= j:
+            if(s[i] != s[j]):
+                palindrome = False
+                break
+            i += 1
+            j -= 1
+        return palindrome
