6bf523a70f986999f70395f43b93958802fe98b3
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-09-17 21:49:43 +0530

    Time: 0 ms (100%), Space: 18 MB (23.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -17,6 +17,8 @@ class Solution:
         result = []
 
         combination = []
+        # Since we have to loop through all digits and then again loop through each letter corresponding to each digit,
+        # we will need to use a loop along with recursion.
         def dfs(i: int):
             if len(combination) == len(digits):
                 c = "".join(combination)
@@ -25,12 +27,19 @@ class Solution:
                 return
 
             letters = self.digitToLettersMap.get(digits[i])
+            # Take each letter from current digit and search for different combinations in further digits.
             for j in range(len(letters)):
                 combination.append(letters[j])
                 dfs(i+1)
+                # backtrack: deselect the previous choice of letters[j]
                 combination.pop()
 
-        
         dfs(0)
         return result
-        
+
+# Time complexity: O(n * 4^n) - while picking a letter for a digit, we have 4 choices in worst case (case of 7 or 9).
+#                               So, number of solutions = number of leaves = 4^height= 4^n.
+#                               And for each solution we will have n letters.
+# Space complexity: O(n) - for recursion
+#                   O(n * 4^n) - for reuslt output
+# where n is lenght of digits
