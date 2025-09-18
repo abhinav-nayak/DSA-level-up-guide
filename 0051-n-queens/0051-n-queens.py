fcc51e6108e56a0f4354683f801a1173a5d54b3b
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-09-18 22:35:13 +0530

    Time: 23 ms (36.36%), Space: 18.5 MB (16.27%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,57 +1,24 @@
-import copy
-
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         # Since we need to find 'all distinct solutions', we can use backtracking
         result = []
+
+        # In each row only 1 queen will be there, and our recursive dfs method will search a posiiton for each row.
+        # So, no need to keep track of invalid rows.
+        # hash set to keep track of invalid columns
+        cols_set = set()
+        # hash set to keep track of invlid positive diagonal. Hint: along every positive diagonal r+c remains constant.
+        pos_diagonal_set = set()
+        # hash set to keep track of invalid negative diagonal. Hint: along every negative diagonal r-c remains constant.
+        neg_diagonal_set = set()
         
         # Initialise a possible solution with all 0's
-        self.solution = [[0 for _ in range(n)] for _ in range(n)]
+        self.solution = [["." for _ in range(n)] for _ in range(n)]
 
         def dfs(i: int):
             """
             i: row number of chess board for which we are finding queen
             """
-            def markPositionsInvalid(r: int, c: int):
-                """
-                r: current row number
-                c: current column number
-                """
-                # mark the entire row as invalid
-                for z in range(n):      
-                    self.solution[r][z] = "."
-
-                # mark the entire column as invalid
-                for z in range(n):      
-                    self.solution[z][c] = "."
-
-                # mark the diagonals as invalid
-                # process right down diagonal
-                drow, dcolumn = r, c
-                while 0 <= drow <n and 0 <= dcolumn < n:
-                    self.solution[drow][dcolumn] = "."
-                    drow += 1
-                    dcolumn +=1
-                # process left up diagonal
-                drow, dcolumn = r, c
-                while 0 <= drow <n and 0 <= dcolumn < n:
-                    self.solution[drow][dcolumn] = "."
-                    drow -= 1
-                    dcolumn -=1
-                # process right up diagonal
-                drow, dcolumn = r, c
-                while 0 <= drow <n and 0 <= dcolumn < n:
-                    self.solution[drow][dcolumn] = "."
-                    drow -= 1
-                    dcolumn +=1
-                # process left down diagonal         
-                drow, dcolumn = r, c
-                while 0 <= drow <n and 0 <= dcolumn < n:
-                    self.solution[drow][dcolumn] = "."
-                    drow += 1
-                    dcolumn -=1
-            
-            # --- DFS method starts here --- #
             print("------- Searching Queen position for row: ", i, "--------")
             if i >= n:
                 # We have found positions for all n queens
@@ -64,19 +31,24 @@ class Solution:
 
             # Check possibility of placing queen in each of the positions in the given row
             for j in range(n):
-                # print("solution: ", self.solution)
-                # If the position (i, j) is valid, select posiiton (i, j)
-                if self.solution[i][j] != ".":
-                    print("Proceeding with position: ", i, ", ", j)
-                    # Queen can be placed at (i, j)
-                    soln_before_choice = copy.deepcopy(self.solution)
-                    # Due to placement of this queen, mark new invalid positions so that queens won't clash
-                    markPositionsInvalid(i, j)
-                    self.solution[i][j] = "Q"
-                    # move forward to find next row queen position
-                    dfs(i+1)
-                    # backtrack by de-selecting the prev choice and reverting marked invalid positions
-                    self.solution = soln_before_choice
+                # If the position (i, j) is invalid, skip it
+                if j in cols_set or i+j in pos_diagonal_set or i-j in neg_diagonal_set:
+                    continue
+
+                print("Proceeding with position: ", i, ", ", j)
+                # Queen can be placed at (i, j)
+                # Due to placement of this queen, mark new invalid positions so that queens won't clash
+                cols_set.add(j)
+                pos_diagonal_set.add(i+j)
+                neg_diagonal_set.add(i-j)
+                self.solution[i][j] = "Q"
+                # move forward to find next row queen position
+                dfs(i+1)
+                # backtrack by de-selecting the prev choice and reverting marked invalid positions
+                cols_set.remove(j)
+                pos_diagonal_set.remove(i+j)
+                neg_diagonal_set.remove(i-j)
+                self.solution[i][j] = "."                    
 
         dfs(0)
         return result
