67560b63f086130b3639132780fcdeddfb58edb7
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-12-03 23:26:38 +0530

    Time: 4 ms (38.17%), Space: 17.8 MB (62.81%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,10 +1,26 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        """
+        We know that the min cost required to reach the top from last 2 steps is their own cost 
+        (this is the base condition). Min cost to reach from a step is cost of current step + min
+        (min cost from next step to reach top, min cost from next to next step to reach top).
+        This satisifes optimal substructure. And if you draw the recursion tree you will see it
+        satisfies overlapping solution as well. Hence, this can be solved in Dynamic Programming.
+        With the above mentioned logic, this can be solved using Memoization and Tabulation 
+        technique both. Tabulation technique requires only 2 pointers and is space efficient.
+        """
         # Using tabulation DP implementation technique
         # only 2 pointers are enough for tabulation method, making it space efficient
+
+        # min cost required to reach the top from last 2 steps is their own cost
         cost1, cost2 = cost[len(cost)-2], cost[len(cost)-1]
         for i in range(len(cost)-3, -1, -1):
+            # Min cost to reach from a step is cost of current step + min(min cost from next step 
+            # to reach top, min cost from next to next step to reach top).
             temp = cost1
             cost1 = cost[i] + min(cost1, cost2)
             cost2 = temp
         return min(cost1, cost2)
+
+# Time complexity: O(n)
+# Space complexity: O(1)
