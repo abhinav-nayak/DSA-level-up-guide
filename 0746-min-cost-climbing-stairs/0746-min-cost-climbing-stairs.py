0c1cd338279639b6877b50e2eece0789c00e39b6
abhinav-nayak <85622931+abhinav-nayak@users.noreply.github.com>
2025-12-03 23:38:48 +0530

    Time: 4 ms (38.17%), Space: 18 MB (29.15%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -6,6 +6,7 @@ class Solution:
         (min cost from next step to reach top, min cost from next to next step to reach top).
         This satisifes optimal substructure. And if you draw the recursion tree you will see it
         satisfies overlapping solution as well. Hence, this can be solved in Dynamic Programming.
+        This is similar to Fibonacci series pattern.
         With the above mentioned logic, this can be solved using Memoization and Tabulation 
         technique both. Tabulation technique requires only 2 pointers and is space efficient.
         """
